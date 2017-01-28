48e18c1e07d176366fe21ebfc24db54aff9314fc
Valentin BROZIN <valentinbrozin@gmail.com>
2017-01-28 19:30:33 +0100

    Ca marche maintenant

--- Assets/CoordinateFinder.cs
+++ Assets/CoordinateFinder.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Xml;
+﻿
 using UnityEngine;
 
 public class CoordinateFinder : MonoBehaviour {
@@ -11,6 +8,8 @@ public class CoordinateFinder : MonoBehaviour {
 
     [SerializeField]
     private Transform earth;
+
+    private RequestManager requestManager = new RequestManager();
 
     void Update()
     {
@@ -22,35 +21,21 @@ public class CoordinateFinder : MonoBehaviour {
             {
                 Vector3 cartesianCoord = earth.InverseTransformPoint(hitInfo.point).normalized;
                 Vector2 sphericalCoord = CartesianToSphericalCoordinates(cartesianCoord);
-                ProcessCoord(sphericalCoord);
+                requestManager.RequestCountry(sphericalCoord);
             }
         }
     }
 
-    private void ProcessCoord(Vector2 sphericalCoord)
-    {
-        Debug.Log("Process coord : " + sphericalCoord.ToString());
-        XmlDocument xDoc = new XmlDocument();
-        String coordinate = sphericalCoord.x + "," + sphericalCoord.y;
-        //xDoc.Load("https://maps.googleapis.com/maps/api/geocode/xml?latlng=" + coordinate + "&location_type=ROOFTOP&result_type=street_address&key=YOURAPIKEY");
-
-        //XmlNodeList xNodelst = xDoc.GetElementsByTagName("result");
-        //XmlNode xNode = xNodelst.Item(0);
-        //string adress = xNode.SelectSingleNode("formatted_address").InnerText;
-        //string mahalle = xNode.SelectSingleNode("address_component[3]/long_name").InnerText;
-        //string ilce = xNode.SelectSingleNode("address_component[4]/long_name").InnerText;
-        //string il = xNode.SelectSingleNode("address_component[5]/long_name").InnerText;
-
-        //Debug.Log(adress + "  " + mahalle + "  " + ilce + "  " + il);
-    }
+    
 
     private Vector2 CartesianToSphericalCoordinates(Vector3 cartesianCoord)
     {
-        Debug.Log("Transform coord : " + cartesianCoord.ToString());
         Vector2 result = new Vector2();
-        
-        result.x = Mathf.Acos(cartesianCoord.y) * Mathf.Rad2Deg;
-        result.y = Mathf.Atan(-cartesianCoord.x / cartesianCoord.z)  * Mathf.Rad2Deg;
+
+        result.x = Mathf.Asin(cartesianCoord.y) * Mathf.Rad2Deg;
+        result.y = Mathf.Atan2(-cartesianCoord.x, cartesianCoord.z)  * Mathf.Rad2Deg;
+
         return result;
     }
+
 }
