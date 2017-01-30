185dd43f9dc4c699202739c2b7a5434479b73383
Audrey Fontanet <audrey-fontanet@hotmail.fr>
2017-01-30 23:21:20 +0100

    unversion project files, fix math error, draft ui, weather api call -not working right now

---
+++ Assets/Earth/Scripts/Data/WeatherData.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeatherData {
+	private string city;
+	private string weather;
+
+	public string City {
+		get {
+			return city;
+		}
+		set {
+			city = value;
+		}
+	}
+
+	public string Weather {
+		get {
+			return weather;
+		}
+		set {
+			weather = value;
+		}
+	}
+}
---
+++ Assets/Earth/Scripts/MathsUtils/CoordinateFinder.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+// Maths utils to handle cartesian and spherical coordiantes frame
+public class CoordinateFinder : MonoBehaviour {
+
+	/// <summary>
+	/// Cartesians to spherical coordinates.
+	/// </summary>
+	/// <returns>The to spherical coordinates.</returns>
+	/// <param name="cartesianCoord">Cartesian coordinate.</param>
+	public static Vector2 CartesianToSphericalCoordinates(Vector3 cartesianCoord)
+    {
+        Vector2 result = new Vector2();
+		result.x = Mathf.Asin(cartesianCoord.y) * Mathf.Rad2Deg;
+		result.y = Mathf.Atan2(-cartesianCoord.x, cartesianCoord.z) * Mathf.Rad2Deg;
+        return result;
+    }
+}
